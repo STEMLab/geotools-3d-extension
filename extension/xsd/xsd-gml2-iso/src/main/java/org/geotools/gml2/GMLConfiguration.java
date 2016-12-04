0fd25f8175a994a29bdeb43f7945d355fd5a1629
hgryoo <hgryoo@hgryoo-laptop>
2016-12-05 01:21:58 +0900

    changed gml2 configuration and parsing DirectPosition and Point in ISO Geometry

--- extension/xsd/xsd-gml2-iso/src/main/java/org/geotools/gml2/GMLConfiguration.java
+++ extension/xsd/xsd-gml2-iso/src/main/java/org/geotools/gml2/GMLConfiguration.java
@@ -19,6 +19,7 @@ package org.geotools.gml2;
 import javax.xml.namespace.QName;
 
 import org.geotools.feature.DefaultFeatureCollections;
+import org.geotools.geometry.GeometryBuilder;
 import org.geotools.gml2.bindings.GMLAbstractFeatureCollectionBaseTypeBinding;
 import org.geotools.gml2.bindings.GMLAbstractFeatureCollectionTypeBinding;
 import org.geotools.gml2.bindings.GMLAbstractFeatureTypeBinding;
@@ -220,6 +221,7 @@ public class GMLConfiguration extends Configuration {
         container.registerComponentInstance(CoordinateSequenceFactory.class,
             CoordinateArraySequenceFactory.instance());
         container.registerComponentImplementation(GeometryFactory.class);
+        container.registerComponentImplementation(GeometryBuilder.class);
         container.registerComponentImplementation(DefaultFeatureCollections.class);
 
         container.registerComponentInstance(srsSyntax);

5c2de6cf7e9a83183ea5f516c59c05e8a88cff03
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-02-21 21:04:01 +0900

    added sudo to build b2

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -3,7 +3,7 @@
 #BOOST
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
 tar -xvf boost-1.60.tar.gz
-cd boost_1_60_0 && ./bootstrap.sh && ./b2 install && cd ..
+cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install && cd ..
 
 #CGAL
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz

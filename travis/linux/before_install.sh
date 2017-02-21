80b65a4dd606443e2614a37a2aa9fce7d9c8b4b4
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-02-21 20:10:11 +0900

    fixed before_install.sh

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -1,19 +1,13 @@
 export DEBIAN_FRONTEND=noninteractive
 
 #BOOST
-sudo add-apt-repository ppa:apokluda/boost1.53 --yes
-sudo add-apt-repository ppa:kalakris/cmake --yes # CMAKE 2.8.11
-sudo apt-get update -qq
-sudo apt-get install --force-yes \
-    cmake libboost-chrono1.53-dev libboost-program-options1.53-dev libboost-filesystem1.53-dev libboost-timer1.53-dev \
-    libboost-test1.53-dev libboost-date-time1.53-dev libboost-thread1.53-dev \
-    libboost-system1.53-dev libboost-serialization1.53-dev \
-    libmpfr-dev libgmp-dev \
-    cmake
+wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
+tar -xvf boost-1.60.tar.gz
+cd boost_1_60_0 && ./bootstrap.sh && ./b2 install && cd ..
 
 #CGAL
-wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz
-tar xzf CGAL-4.7.tar.gz
+wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz
+tar -xvf CGAL-4.7.tar.gz
 cd CGAL-4.7 && cmake . && make && sudo make install && cd ..
 
 #SFCGAL

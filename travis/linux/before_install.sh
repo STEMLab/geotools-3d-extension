6080b5ac18dbe446bce2e9fbbb8a18d063eb697b
Ryoo Hyung-Gyu <hyunggyu.ryoo@gmail.com>
2017-02-22 13:42:46 +0900

    Update before_install.sh
    eliminate -d0 option

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 #BOOST
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
 tar xzf boost-1.60.tar.gz
-cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install -d0 && cd ..
+cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install && cd ..
 
 #CGAL
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz

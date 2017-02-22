f45d72b4f6ed9bd06741a4d3bf87ee6d347134d5
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-02-23 00:09:21 +0900

    eliminate make when installing CGAL

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -8,12 +8,12 @@ cd cmake-3.4.3 && ./bootstrap && sudo make install && cd ..
 #BOOST
 wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
 tar xzf boost-1.60.tar.gz
-cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install && cd ..
+cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/ && cd ..
 
 #CGAL
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz
 tar xzf CGAL-4.7.tar.gz
-cd CGAL-4.7 && cmake . -DWITH_examples=ON -DWITH_demos=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/ && make && sudo make install && cd ..
+cd CGAL-4.7 && cmake . -DWITH_examples=ON -DWITH_demos=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/ && sudo make install && cd ..
 
 #SFCGAL
 wget https://github.com/Oslandia/SFCGAL/archive/v1.3.0.tar.gz -c -O sfcgal-1.3.tar.gz

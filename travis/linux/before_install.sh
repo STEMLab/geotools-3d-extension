9bbe9801c621bd6ffed89323556548bdcf2b7cd0
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-02-23 13:57:51 +0900

    changed cgal version to 4.3

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -11,9 +11,9 @@ tar xzf boost-1.60.tar.gz
 cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/ && cd ..
 
 #CGAL
-wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz
-tar xzf CGAL-4.7.tar.gz
-cd CGAL-4.7 && cmake . -DWITH_examples=ON -DWITH_demos=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/ && sudo make install && cd ..
+wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.3/CGAL-4.3.tar.gz -c -O CGAL-4.3.tar.gz
+tar xzf CGAL-4.3.tar.gz
+cd CGAL-4.3 && cmake . -DWITH_examples=ON -DWITH_demos=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/ && sudo make install && cd ..
 
 #SFCGAL
 wget https://github.com/Oslandia/SFCGAL/archive/v1.3.0.tar.gz -c -O sfcgal-1.3.tar.gz

4a5c0453721f12f43dac0a12d5282157850a1302
Taehoom Kim <taehoon.kim@pusan.ac.kr>
2017-02-24 18:11:24 +0900

    Change LineString merge algorithm for index problem

--- unsupported/geometry-ng/src/main/java/org/geotools/geometry/iso/coordinate/LineStringImpl.java
+++ unsupported/geometry-ng/src/main/java/org/geotools/geometry/iso/coordinate/LineStringImpl.java
@@ -122,11 +122,18 @@ public class LineStringImpl extends CurveSegmentImpl implements LineString {
         LineStringImpl result;
 
         if (other.getEndPoint().equals(this.getStartPoint())) {
-            LinkedList<Position> posToAdd = new LinkedList<Position>(other.getControlPoints());
-            posToAdd.removeLast();
-            posToAdd.addAll(getControlPoints());
-            result = new LineStringImpl( posToAdd );
-
+        	if(this.getEndPoint().equals(other.getStartPoint())){
+        		LinkedList<Position> posToAdd = new LinkedList<Position>(getControlPoints());
+                posToAdd.removeLast();
+                posToAdd.addAll(other.getControlPoints());
+                result = new LineStringImpl( posToAdd );
+        	}
+        	else{
+        		LinkedList<Position> posToAdd = new LinkedList<Position>(other.getControlPoints());
+                posToAdd.removeLast();
+                posToAdd.addAll(getControlPoints());
+                result = new LineStringImpl( posToAdd );	
+        	}
         } else if (this.getEndPoint().equals(other.getStartPoint())) {
             LinkedList<Position> posToAdd = new LinkedList<Position>(getControlPoints());
             posToAdd.removeLast();

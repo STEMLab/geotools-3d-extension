3a33d7931c7dfc0018cc4c03682228c0c4aa1610
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-03-10 17:03:18 +0900

    fixed SFWrapperTest

--- unsupported/geometry-ng/src/test/java/org/geotools/geometry/iso/sfcgal/util/SFWrapperTest.java
+++ unsupported/geometry-ng/src/test/java/org/geotools/geometry/iso/sfcgal/util/SFWrapperTest.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-package org.geotools.geometry.iso.sfcgal.wrapper;
+package org.geotools.geometry.iso.sfcgal.util;
 
 import java.util.ArrayList;
 

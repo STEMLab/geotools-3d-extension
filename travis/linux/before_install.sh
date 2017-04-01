b730b1809439f7c3a5ea574678a39ddc163679f9
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-04-01 15:23:36 +0900

    added trusy boost and cmake

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -5,10 +5,10 @@
 #cd ../
 
 #BOOST
-wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
-tar xzf boost-1.60.tar.gz
-cd boost_1_60_0 && sudo ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/
-cd ../
+#wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
+#tar xzf boost-1.60.tar.gz
+#cd boost_1_60_0 && sudo ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/
+#cd ../
 
 #CGAL
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz

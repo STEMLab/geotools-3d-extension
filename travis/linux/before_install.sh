d1448e67cb25aab37c5725f7ee7a4c8e2fd03ffa
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-04-01 14:18:54 +0900

    fix installing cgal in travis

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -1,5 +1,3 @@
-export DEBIAN_FRONTEND=noninteractive
-
 #CMAKE
 #wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz -O cmake-3.4.3.tar.gz
 #tar xzf cmake-3.4.3.tar.gz
@@ -11,14 +9,13 @@ export DEBIAN_FRONTEND=noninteractive
 #cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/ && cd ../
 
 #CGAL
-
-#wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz
-#tar xzf CGAL-4.7.tar.gz
-#cd CGAL-4.7 && cmake . -DWITH_examples=ON -DWITH_demos=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/ && sudo make install && cd ../
+wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz
+tar xzf CGAL-4.7.tar.gz
+cd CGAL-4.7 && cmake . && make &&  make install && cd ../
 
 #SFCGAL
 wget https://github.com/Oslandia/SFCGAL/archive/v1.3.0.tar.gz -c -O sfcgal-1.3.tar.gz
 tar xzf sfcgal-1.3.tar.gz
-cd SFCGAL-1.3.0 && cmake . ""-DCMAKE_INSTALL_PREFIX=.."" -DCMAKE_BUILD_TYPE=Release -DSFCGAL_CHECK_VALIDITY=ON -DSFCGAL_BUILD_EXAMPLES=ON && sudo make install && cd ../
+cd SFCGAL-1.3.0 && cmake . && make && make install && cd ../
 
 # cmake --version

0ab1694e7b22d8d29b9fc6c04a2e2e1adac5e087
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2017-04-01 14:23:52 +0900

    fix installing cgal in travis

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -1,12 +1,12 @@
 #CMAKE
-#wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz -O cmake-3.4.3.tar.gz
-#tar xzf cmake-3.4.3.tar.gz
-#cd cmake-3.4.3 && ./bootstrap && sudo make install && cd ../
+wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz -O cmake-3.4.3.tar.gz
+tar xzf cmake-3.4.3.tar.gz
+cd cmake-3.4.3 && ./bootstrap && make && make install && cd ../
 
 #BOOST
-#wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
-#tar xzf boost-1.60.tar.gz
-#cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/ && cd ../
+wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download -c -O boost-1.60.tar.gz
+tar xzf boost-1.60.tar.gz
+cd boost_1_60_0 && ./bootstrap.sh && sudo ./b2 install --prefix=/usr/local/ && cd ../
 
 #CGAL
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz -c -O CGAL-4.7.tar.gz

fede5cf2ce3d00da868605f934e1bfd73c784f25
soojinKim <rlatnwls0611@gmail.com>
2017-07-25 19:34:43 +0900

    quick start

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ release = u'2.7.1'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
-
+highlight_language = 'java'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path

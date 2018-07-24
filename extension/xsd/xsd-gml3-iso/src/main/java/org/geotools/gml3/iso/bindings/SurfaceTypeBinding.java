1d19312b72740f6365c662c0e13683261efaeb2b
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2018-07-24 18:57:48 +0900

    fixed getting polygon patches by an element name

--- extension/xsd/xsd-gml3-iso/src/main/java/org/geotools/gml3/iso/bindings/SurfaceTypeBinding.java
+++ extension/xsd/xsd-gml3-iso/src/main/java/org/geotools/gml3/iso/bindings/SurfaceTypeBinding.java
@@ -125,7 +125,7 @@ public class SurfaceTypeBinding extends AbstractComplexBinding implements Compar
      * @generated modifiable
      */
     public Object parse(ElementInstance instance, Node node, Object value) throws Exception {
-    	List polys = node.getChildValues(Polygon.class);
+    	List polys = node.getChildValues("patches");
     	return gb.createSurface(polys);
     }
 
